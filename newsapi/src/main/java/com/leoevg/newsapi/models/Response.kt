3c59ccaf5e563ed5663681708ccfe30511f59b06
ShemLeon <shemiakinleonid@gmail.com>
2025-08-09 12:44:51 +0300

    asd

---
+++ newsapi/src/main/java/com/leoevg/newsapi/models/Response.kt
@@ -0,0 +1,15 @@
+package com.leoevg.newsapi.models
+
+/**
+ * "status": "ok",
+ * "totalResults": 218,
+ * +"articles": [ _ ]
+ */
+
+class Response<E>(
+    status: String,
+    totalResults: Int,
+    articles: List<Article>
+) {
+
+}
